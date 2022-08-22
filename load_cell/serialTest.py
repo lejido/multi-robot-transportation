1dbe94b7bfd878765bb95c596a827ea884d18545
quorimo <jimmolee1226@gmail.com>
2022-08-22 17:48:55 -0400

    asdf

---
+++ load_cell/serialTest.py
@@ -0,0 +1,18 @@
+import serial
+
+prt = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_AB0JPNGI-if00-port0"
+
+ser = serial.Serial(prt) 
+ser.timeout = 1.0
+ser.flushInput() 
+
+while 1:
+  print("in1")
+  ser_bytes = ser.readline() 
+  print("in2")
+  weight = (ser_bytes.decode())
+  print("in3")
+  w = weight.strip()
+  print("in4")
+  print(w)
+  print("out")
