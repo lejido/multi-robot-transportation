8641a39ed301a092016f5a97bc78e83f842eb6a7
quorimo <jimmolee1226@gmail.com>
2022-09-08 18:01:58 -0400

    asdf

--- load_cell/loadcellPublisher.py
+++ load_cell/loadcellPublisher.py
@@ -3,18 +3,21 @@
 import rospy 
 import serial
 
-from std_msgs.msg import Float32 
+from std_msgs.msg import String
 
-ser = serial.Serial('/dev/ttyACM0')
-ser.flushInput()
+prt = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_AB0JPNGI-if00-port0"
+
+ser = serial.Serial(prt)
+# ser.timeout = 1.0
+ser.flushInput() #get rid of unwanted input stuff  
 
 def talker():
-  pub = rospy.Publisher("loadcell", Float32, queue_size=10) 
+  pub = rospy.Publisher("loadcell", String, queue_size=10) 
   rospy.init_node("loadcellPublisher", anonymous=True)
   rate = rospy.Rate(10)
   while not rospy.is_shutdown():
     ser_bytes = ser.readline() 
-    weight = ser_bytes.decode() 
+    weight = (ser_bytes.decode()).strip() 
     rospy.loginfo("Weight: " + weight + "g") 
     pub.publish(weight) 
     rate.sleep() 
--- load_cell/loadcellSubscriber.py
+++ load_cell/loadcellSubscriber.py
@@ -1,12 +1,12 @@
 import rospy 
-from std_msgs.msg import Float32
+from std_msgs.msg import String
 
 def callback(data):
-  rospy.loginfo(rospy.get_caller_id() + "Weight: %lf grams", data.data) 
+  rospy.loginfo(rospy.get_caller_id() + "Weight: %s grams", data.data) 
 
 def listener():
   rospy.init_node('loadcellSubscriber', anonymous=True)
-  rospy.Subscriber("loadcell", Float32, callback) 
+  rospy.Subscriber("loadcell", String, callback) 
   rospy.spin() 
 
 if __name__ == '__main__':
